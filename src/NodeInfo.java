351e61575fb1c2d7a2f71cd105e211762fda830b
Niall Mulloy <nmulloy@tcd.ie>
2017-10-30 22:26:53 +0000

    BFS update
    BFS update

--- src/NodeInfo.java
+++ src/NodeInfo.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 public class NodeInfo {
 		Object val;
-		private String colour = "white";
+		String colour = "white";
 		int index;
 		ArrayList<NodeInfo> adj;
+		int count;
 		
 		public NodeInfo(Object val, int index){
 			this.val = val;
 			this.index = index;
 			this.adj = new ArrayList<NodeInfo>();
+			this.count = 0;
 		}
 	
 }
