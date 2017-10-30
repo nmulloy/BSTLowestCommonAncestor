756e42f0dae792d818ebcb967ed20f4853a54eef
Niall Mulloy <nmulloy@tcd.ie>
2017-10-30 17:44:48 +0000

    Added BFS
    BFS

--- src/DAG.java
+++ src/DAG.java
@@ -74,14 +74,30 @@ public class DAG{
 	}
 	
 	  public static void main(String[] args) {
-		  DAG graph = new DAG(5);
-		  graph.addEdge(3, 2);
+	
+		  DAG graph = new DAG(13);
 		  graph.addEdge(4, 2);
-		  graph.addEdge(1, 3);
-		  graph.addEdge(0, 2);
-		  graph.addEdge(1, 2);
-		  graph.addEdge(2, 1);
-		  graph.addEdge(3, 4);
+		  graph.addEdge(2, 3);
+		  graph.addEdge(3, 2);
+		  graph.addEdge(6, 0);
+		  graph.addEdge(0, 1);
+		  graph.addEdge(2, 0);
+		  graph.addEdge(11, 12);
+		  graph.addEdge(12, 9);
+		  graph.addEdge(9, 10);
+		  graph.addEdge(9, 11);
+		  graph.addEdge(10, 12);
+		  graph.addEdge(11, 4);
+		  graph.addEdge(4, 3);
+		  graph.addEdge(3, 5);
+		  graph.addEdge(6, 8);
+		  graph.addEdge(8, 6);
+		  graph.addEdge(5, 4);
+		  graph.addEdge(0, 5);
+		  graph.addEdge(6, 4);
+		  graph.addEdge(6, 9);
+		  graph.addEdge(7, 6);
+		  
 		  System.out.println(graph.toString());
 	  }
 }
